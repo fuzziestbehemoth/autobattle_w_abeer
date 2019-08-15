d62f13307d042b6c7800b8065f3057aa5a8b796d
fuzziestbehemoth <51346511+fuzziestbehemoth@users.noreply.github.com>
2019-08-14 22:18:21 -0600

    Add files via upload

---
+++ unit_class.py
@@ -0,0 +1,25 @@
+# Created by: Abeer and Ander
+# Date: 8/14/19
+# Objective: Hold data on units
+
+def class Unit(career):
+    self.career = career
+    self.type = ''
+    
+    self.health = 0
+    
+    self.speed = 0
+    self.mregen = 0
+    
+    self.armor = 0
+    self.mresist = 0
+    
+    self.attack = 0
+    self.mattack = 0
+    
+    self.exp = 0
+    self.level = 0
+    
+    self.attacks = []
+    
+    
